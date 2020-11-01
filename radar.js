ff15135f876a02cb1af61e3b655390910e4563a3
danhayes <danhayes3008@outlook.com>
2020-11-01 11:32:18 +0000

    added some lines to identify the horizontal and vertical lines of the radar

--- radar.js
+++ radar.js
@@ -6,3 +6,14 @@ canvas.width = 300;
 canvas.height = 300;
 
 var c = canvas.getContext("2d");
+
+// sets the horizontal and virtical lines of the radar
+c.beginPath();
+c.moveTo(150, 0);
+c.lineTo(150, 300);
+c.stroke();
+
+c.beginPath();
+c.moveTo(0, 150);
+c.lineTo(300, 150);
+c.stroke();
