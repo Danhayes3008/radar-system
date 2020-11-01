09ea170cc9e91627c9f8499c456737f5f61fe9d9
danhayes <danhayes3008@outlook.com>
2020-11-01 11:38:17 +0000

    added the arc's that indicate the distance from the center of the radar

--- radar.js
+++ radar.js
@@ -17,3 +17,29 @@ c.beginPath();
 c.moveTo(0, 150);
 c.lineTo(300, 150);
 c.stroke();
+
+// sets the distance radius markers on the radar
+c.beginPath();
+// 30km distance
+c.arc(150, 150, 30, 0, 2 * Math.PI);
+c.stroke();
+c.beginPath();
+// 50km distance
+c.arc(150, 150, 50, 0, 2 * Math.PI);
+c.stroke();
+c.beginPath();
+// 70km distance
+c.arc(150, 150, 70, 0, 2 * Math.PI);
+c.stroke();
+c.beginPath();
+// 90km distance
+c.arc(150, 150, 90, 0, 2 * Math.PI);
+c.stroke();
+c.beginPath();
+// 110km distance
+c.arc(150, 150, 110, 0, 2 * Math.PI);
+c.stroke();
+c.beginPath();
+// 130km distance
+c.arc(150, 150, 130, 0, 2 * Math.PI);
+c.stroke();
